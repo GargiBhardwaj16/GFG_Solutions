210d834a6e61f99034ad4cd18238099c4f5f5306
Gargi Bhardwaj <102040886+GargiBhardwaj16@users.noreply.github.com>
2024-04-02 10:38:59 +0530

    Added Solution - GfG to GitHub

---
+++ Medium/Kadane's Algorithm/kadanes-algorithm.java
@@ -0,0 +1,56 @@
+//{ Driver Code Starts
+import java.io.*;
+
+import java.util.*;
+
+class Main {
+    
+	public static void main (String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		int t = Integer.parseInt(br.readLine().trim()); //Inputting the testcases
+		while(t-->0){
+		    //size of array
+		    int n = Integer.parseInt(br.readLine().trim());
+		    int arr[] = new int[n];
+		    String inputLine[] = br.readLine().trim().split(" ");
+		    
+		    //adding elements
+		    for(int i=0; i<n; i++){
+		        arr[i] = Integer.parseInt(inputLine[i]);
+		    }
+		    
+		    Solution obj = new Solution();
+		    
+		    //calling maxSubarraySum() function
+		    System.out.println(obj.maxSubarraySum(arr, n));
+		}
+	}
+}
+
+
+// } Driver Code Ends
+
+
+class Solution{
+
+    // arr: input array
+    // n: size of array
+    //Function to find the sum of contiguous subarray with maximum sum.
+    long maxSubarraySum(int arr[], int n){
+        
+        // Your code here
+        long currentSum = arr[0];  // Initialize currentSum with the first element of the array
+        long maxSum = arr[0];
+        for (int i = 1; i < n; i++) {
+            // Calculate the maximum between the current element and the sum including the current element
+            currentSum = Math.max(arr[i], currentSum + arr[i]);
+
+            // Update maxSum if the currentSum is greater
+            maxSum = Math.max(maxSum, currentSum);
+        }
+
+        return maxSum;
+    }
+    
+}
+
