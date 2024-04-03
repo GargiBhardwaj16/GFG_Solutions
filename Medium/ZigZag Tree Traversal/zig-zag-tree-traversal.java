209931d75421483863722b85c2190ecfd83edde1
Gargi Bhardwaj <102040886+GargiBhardwaj16@users.noreply.github.com>
2024-04-03 12:34:55 +0530

    Added Solution - GfG to GitHub

---
+++ Medium/ZigZag Tree Traversal/zig-zag-tree-traversal.java
@@ -0,0 +1,170 @@
+//{ Driver Code Starts
+//Initial Template for Java
+
+import java.util.*;
+import java.util.HashMap;
+import java.io.*;
+
+    class Node
+    {
+        int data;
+        Node left,right;
+        Node(int data){
+        this.data = data;
+        left=null;
+        right=null;
+    }
+    }
+		
+public class GFG2
+{
+    static Node buildTree(String str){
+        
+        if(str.length()==0 || str.charAt(0)=='N'){
+            return null;
+        }
+        
+        String ip[] = str.split(" ");
+        // Create the root of the tree
+        Node root = new Node(Integer.parseInt(ip[0]));
+        // Push the root to the queue
+        
+        Queue<Node> queue = new LinkedList<>(); 
+        
+        queue.add(root);
+        // Starting from the second element
+        
+        int i = 1;
+        while(queue.size()>0 && i < ip.length) {
+            
+            // Get and remove the front of the queue
+            Node currNode = queue.peek();
+            queue.remove();
+                
+            // Get the current node's value from the string
+            String currVal = ip[i];
+                
+            // If the left child is not null
+            if(!currVal.equals("N")) {
+                    
+                // Create the left child for the current node
+                currNode.left = new Node(Integer.parseInt(currVal));
+                // Push it to the queue
+                queue.add(currNode.left);
+            }
+                
+            // For the right child
+            i++;
+            if(i >= ip.length)
+                break;
+                
+            currVal = ip[i];
+                
+            // If the right child is not null
+            if(!currVal.equals("N")) {
+                    
+                // Create the right child for the current node
+                currNode.right = new Node(Integer.parseInt(currVal));
+                    
+                // Push it to the queue
+                queue.add(currNode.right);
+            }
+            i++;
+        }
+        
+        return root;
+    }
+    
+	public static void inorder(Node root)
+	{
+	    if(root==null)
+	    return;
+	    inorder(root.left);
+	    System.out.print(root.data);
+	    inorder(root.right);
+	}
+     /* Drier program to test above functions */
+    public static void main(String args[]) throws IOException
+    {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+	        
+	        int t=Integer.parseInt(br.readLine());
+    
+	        while(t > 0){
+	            String s = br.readLine();
+    	    	Node root = buildTree(s);
+        	    GFG g = new GFG();
+			
+			    ArrayList<Integer> res = g.zigZagTraversal(root) ;
+			    for (int i = 0; i < res.size (); i++)
+			        System.out.print (res.get (i) + " ");
+			    System. out. println();  
+    			
+                t--;
+            
+        }
+	}
+}
+// } Driver Code Ends
+
+
+//User function Template for Java
+
+/*class Node
+{
+    int data;
+    Node left,right;
+    Node(int d)
+    {
+        data=d;
+        left=right=null;
+    }
+}*/
+
+class GFG
+{
+    //Function to store the zig zag order traversal of tree in a list.
+	ArrayList<Integer> zigZagTraversal(Node root)
+	{
+	    //Add your code here.
+	    ArrayList<Integer> result = new ArrayList<>();
+
+        if (root == null) return result;
+
+        // Using a queue for level order traversal
+        Queue<Node> queue = new LinkedList<>();
+        queue.offer(root);
+
+        // Flag to indicate the direction of traversal
+        boolean leftToRight = true;
+
+        while (!queue.isEmpty()) {
+            int levelSize = queue.size();
+            ArrayList<Integer> currentLevel = new ArrayList<>();
+
+            // Traverse current level
+            for (int i = 0; i < levelSize; i++) {
+                Node current = queue.poll();
+
+                // Add node data to current level list based on traversal direction
+                if (leftToRight) {
+                    currentLevel.add(current.data);
+                } else {
+                    currentLevel.add(0, current.data);
+                }
+
+                // Enqueue child nodes for the next level
+                if (current.left != null) queue.offer(current.left);
+                if (current.right != null) queue.offer(current.right);
+            }
+
+            // Add current level to the result list
+            result.addAll(currentLevel);
+
+            // Reverse the direction for the next level
+            leftToRight = !leftToRight;
+        }
+
+        return result;
+	}
+}
