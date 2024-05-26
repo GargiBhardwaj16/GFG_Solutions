107f6e1445f2b2b1e2c8ea3855d29a5116bf7b58
Gargi Bhardwaj <102040886+GargiBhardwaj16@users.noreply.github.com>
2024-05-26 12:04:53 +0530

    Added Solution - GfG to GitHub

---
+++ Medium/Minimum Spanning Tree/minimum-spanning-tree.java
@@ -0,0 +1,117 @@
+//{ Driver Code Starts
+
+
+import java.io.*;
+import java.lang.*;
+import java.util.*;
+
+public class Main {
+    static BufferedReader br;
+    static PrintWriter ot;
+
+    public static void main(String args[]) throws IOException {
+        br = new BufferedReader(new InputStreamReader(System.in));
+        ot = new PrintWriter(System.out);
+        int t = Integer.parseInt(br.readLine().trim());
+        while (t-- > 0) {
+            String s[] = br.readLine().trim().split(" ");
+            int V = Integer.parseInt(s[0]);
+            int E = Integer.parseInt(s[1]);
+            List<List<int[]>> list = new ArrayList<>();
+            for (int i = 0; i < V; i++) list.add(new ArrayList<>());
+            for (int i = 0; i < E; i++) {
+                s = br.readLine().trim().split(" ");
+                int a = Integer.parseInt(s[0]);
+                int b = Integer.parseInt(s[1]);
+                int c = Integer.parseInt(s[2]);
+                list.get(a).add(new int[] {b, c});
+                list.get(b).add(new int[] {a, c});
+            }
+            ot.println(new Solution().spanningTree(V, E, list));
+        }
+        ot.close();
+    }
+}
+// } Driver Code Ends
+
+
+class Edge implements Comparable<Edge> {
+    int src, dest, weight;
+
+    Edge(int src, int dest, int weight) {
+        this.src = src;
+        this.dest = dest;
+        this.weight = weight;
+    }
+
+    @Override
+    public int compareTo(Edge other) {
+        return this.weight - other.weight;
+    }
+}
+
+class DisjointSet {
+    int[] parent, size;
+
+    DisjointSet(int n) {
+        parent = new int[n];
+        size = new int[n];
+        for (int i = 0; i < n; i++) {
+            parent[i] = i;
+            size[i] = 1;
+        }
+    }
+
+    int findUPar(int node) {
+        if (node == parent[node]) {
+            return node;
+        }
+        return parent[node] = findUPar(parent[node]);
+    }
+
+    void unionBySize(int u, int v) {
+        int rootU = findUPar(u);
+        int rootV = findUPar(v);
+        if (rootU != rootV) {
+            if (size[rootU] < size[rootV]) {
+                parent[rootU] = rootV;
+                size[rootV] += size[rootU];
+            } else {
+                parent[rootV] = rootU;
+                size[rootU] += size[rootV];
+            }
+        }
+    }
+}
+
+class Solution {
+    static int spanningTree(int V, int E, List<List<int[]>> adj) {
+        List<Edge> edges = new ArrayList<>();
+        
+        for (int i = 0; i < V; i++) {
+            for (int[] edge : adj.get(i)) {
+                int adjNode = edge[0];
+                int wt = edge[1];
+                edges.add(new Edge(i, adjNode, wt));
+            }
+        }
+
+        DisjointSet ds = new DisjointSet(V);
+        Collections.sort(edges);
+
+        int mstWt = 0;
+
+        for (Edge edge : edges) {
+            int u = edge.src;
+            int v = edge.dest;
+            int wt = edge.weight;
+
+            if (ds.findUPar(u) != ds.findUPar(v)) {
+                mstWt += wt;
+                ds.unionBySize(u, v);
+            }
+        }
+
+        return mstWt;
+    }
+}
